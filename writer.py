351aff7d317e5c1790d30dbc4d67890b52275f1d
Oleksiy Protas <elfy.ua@gmail.com>
2017-07-29 21:58:41 +0300

    General layout of the writer thread

--- writer.py
+++ writer.py
@@ -19,3 +19,31 @@
 # See the Licence for the specific language governing
 # permissions and limitations under the Licence.
 #
+
+from time import time, sleep
+
+def run(fd, pids, delay, discovery_mode):
+    """
+    Requests pids from the fd stream infinitely spaced out with at least
+    delay milliseconds.
+
+    If discovery_mode is on, exits after the first run.
+    """
+    while True:
+        # Starting the timer
+        start = time()
+
+        # Requesting all the pids, converting to string representation
+        for pid in pids:
+            print('01' + hex(pid).upper()[2:], file=fd, end='\r')
+
+        # Checking if we need to continue
+        if discovery_mode:
+            break
+
+        # Stopping the timer
+        time_left = delay - 1000 * ( time() - start )
+
+        # Sleeping the leftover
+        if time_left > 0:
+            sleep()
