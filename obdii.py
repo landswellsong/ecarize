768f193062b6f0bfe58498078a5105b0c673f30d
Oleksiy Protas <elfy.ua@gmail.com>
2017-07-29 14:47:57 +0300

    Core PIDs covered

--- obdii.py
+++ obdii.py
@@ -22,6 +22,66 @@
 
 from inspect import signature
 
+def parse(pid, fd):
+    """
+    Reads the response to pid from the connection object fd and converts it into
+    a meaningful value.
+
+    Don't try to read PIDs that this function doesn't support because of the
+    variable length of the responses.
+
+    Extensions should go here, please follow the template. The amount of data
+    bytes to read is deremined by the number of arguments to the formula.
+
+    https://en.wikipedia.org/wiki/OBD-II_PIDs
+    """
+    # Determining the formula to used
+    formula = {
+        # PIDs supported
+        0x00: bit_decoder(0x01),
+        0x20: bit_decoder(0x21),
+        0x40: bit_decoder(0x41),
+
+        # Calculated engine load, %
+        0x04: lambda A: A / 2.55,
+
+        # Engine coolant temperature, °C
+        0x05: lambda A: A - 40,
+
+        # Fuel pressure, kPa
+        0x0A: lambda A: 3 * A,
+
+        # Intake manifold absolute pressure, kPa
+        0x0B: lambda A: A,
+
+        # Engine RPM
+        0x0C: lambda A, B: ( 256 * A + B ) / 4,
+
+        # Vehicle speed, km/h
+        0x0D: lambda A: A,
+
+        # Timing advance, °
+        0x0E: lambda A: A / 2 - 64,
+
+        # Intake air temperature, °C
+        0x0F: lambda A: A - 40,
+
+        # MAF air flow rate, g/s
+        0x10: lambda A, B: ( 256 * A + B ) / 100,
+
+        # Throttle position, %
+        0x11: lambda A: A / 2.55,
+    }[pid]
+
+    # Determining how much data we need to read
+    n_to_read = len(signature(formula).parameters)
+
+    # Reading from the descriptor and passing through the formula
+    v = fd.read(n_to_read)
+
+    return formula(*v)
+
+
 def bit_decoder(base_pid):
     """
     Generates a function to decode 4 bytes of PID support information into
@@ -34,5 +94,7 @@ def bit_decoder(base_pid):
 
 if __name__ == '__main__':
     # Example from Wikipedia
-    foo = bit_decoder(1)
-    print(foo(0xBE, 0x1F, 0xA8, 0x13))
+    foo = bit_decoder(0x01)
+    if foo(0xBE, 0x1F, 0xA8, 0x13)) != ( 0x01, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x13, 0x15, 0x1C, 0x1F, 0x20):
+        print("Bit decoder is broken")
